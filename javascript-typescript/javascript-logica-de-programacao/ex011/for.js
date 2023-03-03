461d9edf2c91a8bc4aa5f4254cd3e4c3dd35caed
Alexandre Retamero Junior <121994672+aleretamero@users.noreply.github.com>
2023-03-02 21:38:44 -0300

    Create for.js

---
+++ javascript-typescript/javascript-logica-de-programacao/ex011/for.js
@@ -0,0 +1,14 @@
+// No for prcisa criar uma variável,
+// criar uma condição para parar o laço,
+// precisa incrementar ou decrementar a variável
+
+//  i = index; break; incrementar
+for (let i = 0; i <= 5; i++) {
+  console.log(`Linha ${i}`);
+}
+
+const frutas = ["Maça", "Pêra", "Uva"];
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
