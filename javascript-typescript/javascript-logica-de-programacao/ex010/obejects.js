3d2574727cf42ee1c2891183f48185cb5cc696d3
Alexandre Retamero Junior <121994672+aleretamero@users.noreply.github.com>
2023-03-02 21:38:42 -0300

    Update obejects.js

--- javascript-typescript/javascript-logica-de-programacao/ex010/obejects.js
+++ javascript-typescript/javascript-logica-de-programacao/ex010/obejects.js
@@ -1,1 +1,20 @@
 // atribuição via desestruturação (objects)
+
+const pessoa = {
+  nome: "Luiz",
+  sobrenome: "Miranda",
+  idade: 30,
+  endereco: {
+    rua: "Av Brtasil",
+    numero: 320,
+  },
+};
+
+// Atribuição via desestruturação
+const {
+  nome: teste = "",
+  sobrenome,
+  endereco: { rua, numero },
+} = pessoa;
+
+console.log(teste, sobrenome, rua, numero);
